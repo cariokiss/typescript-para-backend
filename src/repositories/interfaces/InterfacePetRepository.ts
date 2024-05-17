64a86aa4d38072d6b5f88e48e30553a1669fc242
Victor Ozoterio <victorozoterio@gmail.com>
2024-05-17 20:00:17 -0300

    Finalizando o update e delete do CRUD

--- src/repositories/interfaces/InterfacePetRepository.ts
+++ src/repositories/interfaces/InterfacePetRepository.ts
@@ -3,6 +3,6 @@ import PetEntity from "../../entity/PetEntity";
 export default interface InterfacePetRepository {
     criaPet(pet: PetEntity): void;
     listaPet(): Array<PetEntity> | Promise<PetEntity[]>; // o pipe | é um simbolo de união
-    atualizaPet(id: number, pet: PetEntity): void;
-    deletaPet(id: number, pet: PetEntity): void;
+    atualizaPet(id: number, pet: PetEntity): Promise<{ success: boolean; message?: string }> | void;
+    deletaPet(id: number): Promise<{ success: boolean; message?: string }> | void;
 }
