691faa1fbdd21fa4efe634d877b54f89c6d19a64
Victor Ozoterio <victorozoterio@gmail.com>
2024-05-20 14:26:20 -0300

    Protegendo o Request e o Response do CRUD do adotante como um todo

--- src/tipos/tiposAdotante.ts
+++ src/tipos/tiposAdotante.ts
@@ -1,8 +1,18 @@
 import AdotanteEntity from '../entities/AdotanteEntity';
 
 type TipoRequestBodyAdotante = Omit<AdotanteEntity, 'id'>; //Omit = omite informações
+
+type TipoRequestParamsAdotante = { id?: string };
+
 type TipoResponseBodyAdotante = {
-  data?: Pick<AdotanteEntity, 'id' | 'nome' | 'celular'>; //Pick = seleciona os campos a serem retornados
+  data?:
+    | Pick<AdotanteEntity, 'id' | 'nome' | 'celular'>
+    | Pick<AdotanteEntity, 'id' | 'nome' | 'celular'>[]; //Pick = seleciona os campos a serem retornados
+  error?: unknown;
 };
 
-export { TipoRequestBodyAdotante, TipoResponseBodyAdotante };
+export {
+  TipoRequestBodyAdotante,
+  TipoResponseBodyAdotante,
+  TipoRequestParamsAdotante,
+};
